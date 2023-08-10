bc724949aaea0bedefcb7975667ac0a3587a5993
Shazil0204 <141632242+Shazil0204@users.noreply.github.com>
2023-08-10 09:58:27 +0200

    1. completed
    (Variable og datatyper) is completed

--- 1.Variable og datatyper/1.Variable og datatyper/Program.cs
+++ 1.Variable og datatyper/1.Variable og datatyper/Program.cs
@@ -10,6 +10,31 @@ namespace _1.Variable_og_datatyper
     {
         static void Main(string[] args)
         {
+
+            // Task a
+
+            int length = 16;
+
+            double width = 5.1;
+
+            Console.WriteLine(length + width);
+
+
+
+            // Task b
+
+            int area = width * length;
+
+            // The reason Visual Studio compiler won't translate my code is because area can't hold a double value.
+
+
+
+            // Task c
+
+            double d = width * length;
+
+            // The reason Visual Studio compiler wants to translate my code is because d is a double now and it can hold the value of double.
+
         }
     }
 }
