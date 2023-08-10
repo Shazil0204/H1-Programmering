a99848e9bd7fec1d77ff0b000a4902637e047fae
Shazil0204 <141632242+Shazil0204@users.noreply.github.com>
2023-08-10 13:05:10 +0200

    3 task
    the taske 3 is completed

---
+++ 3.Temperaturberegneren/3.Temperaturberegneren/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.Temperaturberegneren
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please tell me the temperature over there: ");
+
+            float celcius = float.Parse(Console.ReadLine());
+
+            float fahrenheit = (celcius * 1.8f) + 32;
+
+            float reamur = celcius * 0.8f;
+
+            Console.WriteLine("" +
+                "The  temperature you type is " + celcius + " °C\n" +
+                "This temperature in fahrenheit is " + fahrenheit + " °F\n" +
+                "This temperature in reamur is " + reamur + " °Ré");
+
+        }
+    }
+}
