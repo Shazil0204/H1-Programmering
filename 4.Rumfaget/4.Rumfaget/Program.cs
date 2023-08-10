5ef26efa4d3b6be8bf5371d427b8fdf79e3edc70
Shazil0204 <141632242+Shazil0204@users.noreply.github.com>
2023-08-10 13:23:00 +0200

    4 task
    The task 4 is completed

---
+++ 4.Rumfaget/4.Rumfaget/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.Rumfaget
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please type the lenght of the object: ");
+
+            float length = float.Parse(Console.ReadLine());
+
+            Console.Write("Please type the height of the object: ");
+
+            float height = float.Parse(Console.ReadLine());
+
+            Console.Write("Please type the width  of the object: ");
+
+            float width = float.Parse(Console.ReadLine());
+
+            float result = length * height * width;
+
+            Console.WriteLine("The Volume of the object is " + result);
+        }
+    }
+}
