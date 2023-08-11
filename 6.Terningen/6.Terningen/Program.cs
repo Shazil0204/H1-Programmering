a97519b9636c16676fdbdc97ed83411e19a13473
Shazil0204 <141632242+Shazil0204@users.noreply.github.com>
2023-08-11 11:45:21 +0200

    6 task
    the task 6 is completed

---
+++ 6.Terningen/6.Terningen/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6.Terningen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // this is to change the color of console and the text itself
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+
+            // this creates a random number and add the random number have to be between 0 - 7
+            Random random = new Random();
+            int dice = random.Next(1, 7);
+
+
+            // the whole if statement
+            if (dice == 1)
+            {
+                Console.WriteLine("Du slog en " + dice);
+            }
+            else if (dice == 2)
+            {
+                Console.WriteLine("Du slog en " + dice);
+            }
+            else if (dice == 3)
+            {
+                Console.WriteLine("Du slog en " + dice);
+            }
+            else if (dice == 4)
+            {
+                Console.WriteLine("Du slog en " + dice);
+            }
+            else if (dice == 5)
+            {
+                Console.WriteLine("Du slog en " + dice);
+            }
+            else if (dice == 6)
+            {
+                Console.WriteLine("Du slog en " + dice);
+            }
+        }
+    }
+}
