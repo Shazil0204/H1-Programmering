10ebc7e231d2635bd44693ac74d53fbb19563cae
Shazil0204 <141632242+Shazil0204@users.noreply.github.com>
2023-08-11 13:14:59 +0200

    Modified 6 task
    task 6 is modified

--- 6.Terningen/6.Terningen/Program.cs
+++ 6.Terningen/6.Terningen/Program.cs
@@ -10,12 +10,6 @@ namespace _6.Terningen
     {
         static void Main(string[] args)
         {
-
-            // this is to change the color of console and the text itself
-            Console.BackgroundColor = ConsoleColor.Blue;
-            Console.ForegroundColor = ConsoleColor.Yellow;
-
-
             // this creates a random number and add the random number have to be between 0 - 7
             Random random = new Random();
             int dice = random.Next(1, 7);
@@ -24,26 +18,38 @@ namespace _6.Terningen
             // the whole if statement
             if (dice == 1)
             {
+                Console.BackgroundColor = ConsoleColor.DarkRed;
+                Console.ForegroundColor = ConsoleColor.DarkBlue;
                 Console.WriteLine("Du slog en " + dice);
             }
             else if (dice == 2)
             {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Blue;
                 Console.WriteLine("Du slog en " + dice);
             }
             else if (dice == 3)
             {
+                Console.BackgroundColor = ConsoleColor.Yellow;
+                Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("Du slog en " + dice);
             }
             else if (dice == 4)
             {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Yellow;
                 Console.WriteLine("Du slog en " + dice);
             }
             else if (dice == 5)
             {
+                Console.BackgroundColor = ConsoleColor.Blue;
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("Du slog en " + dice);
             }
             else if (dice == 6)
             {
+                Console.BackgroundColor = ConsoleColor.DarkBlue;
+                Console.ForegroundColor = ConsoleColor.DarkRed;
                 Console.WriteLine("Du slog en " + dice);
             }
         }
---
+++ 8.Morsekoden/8.Morsekoden/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8.Morsekoden
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please type something: ");
+
+            string UserInput = (Console.ReadLine());
+
+            char[] UserInputInChar = UserInput.ToCharArray();
+
+            foreach (char c in UserInputInChar)
+            {
+                switch(c)
+                {
+                    case 'a':
+                        Console.WriteLine();
+                        break;
+                }
+            }
+        }
+    }
+}
