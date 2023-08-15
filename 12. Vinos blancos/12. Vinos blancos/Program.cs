ecf02aae06887f66b7b83892309ddd85c7968275
Shazil0204 <141632242+Shazil0204@users.noreply.github.com>
2023-08-15 19:28:20 +0200

    12 task
    The task 12 is completed

--- 12. Vinos blancos/12. Vinos blancos/Program.cs
+++ 12. Vinos blancos/12. Vinos blancos/Program.cs
@@ -13,40 +13,63 @@ namespace _12.Vinos_blancos
         {
             Controller();
         }
-
-        
         static void Controller()
         {
             View();
-            Console.WriteLine(Model()[1, 1]);
+            Model();
             Console.ReadLine();
         }
-
         static void View()
         {
-            //Console.WriteLine("Over here i have a graph from the information given by the database");
-
-            Console.WriteLine("Go and read on you own");
+            Console.WriteLine("Over here i have a graph from the information given by the database");
         }
-
-        static int[,] Model()
+        static int[] Model()
         {
-            int[,] Sale_of_Vine = {  // THIS HAS TO BE ASINGLE ARRAY
-                { 2009, 175134 }, 
-                { 2010, 175388 }, 
-                { 2011, 172818 }, 
-                { 2012, 142709 }, 
-                { 2013, 151437 }, 
-                { 2014, 152620 }, 
-                { 2015, 150979 }, 
-                { 2016, 152210 }, 
-                { 2017, 149450 }, 
-                { 2018, 154398 }, 
-                { 2019, 150160 }  };
+
+            // This is my database
+            int[] Sale_of_Vine = {
+                 175134 , 
+                 175388 , 
+                 172818 , 
+                 142709 , 
+                 151437 , 
+                 152620 , 
+                 150979 , 
+                 152210 , 
+                 149450 , 
+                 154398 , 
+                 150160 };
+
+            Sale_of_Vine[5] = Sale_of_Vine[5] + 35432;
 
             Array.Sort(Sale_of_Vine);
 
+            int max = Sale_of_Vine.Max();
+
+            const byte maxStar = 100;
+            
+            for (int i = 0; i < Sale_of_Vine.Length; i++)
+            {
+                int star = maxStar * Sale_of_Vine[i] / max;
+
+                Dots_Controller(star);
+            }
+
             return Sale_of_Vine;
+        }
+        static void Dots_Controller(int Dots)
+        {
+            for (int i = 0; i < Dots; i++)
+            {
+                View_Dots();
+         
+            }
+            Console.WriteLine("");
+        }
+
+        static void View_Dots()
+        {
+            Console.Write("*");
         }
     }
 }
